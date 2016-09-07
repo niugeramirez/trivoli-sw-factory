820c2c96395a8e1614afcaebe7d5c441b83be673
niugeramirez <niugeramirez@gmail.com>
2016-09-07 14:22:12 -0300

    Scripts de base de datos para impresion de historia clinica parametrizado.

---
+++ gestionTurnosASP/scripts_sql/004 - alter empresa - campos de fonts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE dbo.empresa ADD
+	hist_clin_bold varchar (1) NULL,
+	hist_clin_size varchar (2) NULL,	
+	hist_clin_face varchar (50) NULL
---
+++ gestionTurnosASP/scripts_sql/005 - update empresa - campos de fonts.sql
@@ -0,0 +1,4 @@
+update empresa set
+hist_clin_bold = 'N'
+,hist_clin_size = '2'
+,hist_clin_face = 'Verdana'
